9c741928dca48b0fab70f2a802703e82a96e3d82
Ernie White <ernie.white.68@gmail.com>
2023-08-01 05:17:56 -0500

    added skeleton code

--- 2015/Day_07/main.py
+++ 2015/Day_07/main.py
@@ -1,2 +1,22 @@
 # Advent of Code - 2015
 # --- Day 7: Some Assembly Required ---
+
+def get_input(file_name):
+    with open(file_name) as f:
+        data = f.read().splitlines()
+    return data
+
+def solver_1(data):
+    pass
+
+def solver_2(data):
+    pass
+
+def main():
+    data = get_input('test_input.txt')
+    print(f'Part 1: {solver_1(data)}')
+    print(f'Part 2: {solver_2(data)}')
+
+if __name__ == '__main__':
+    main()
+
